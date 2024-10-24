b743026ed7b3536f6595202874adbcbbbf4f8ae6
Adriano <adrianoact@msn.com>
2024-10-24 20:26:28 -0300

    Automated testing: get the summary test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -81,5 +81,35 @@ describe('Transactions routes', () => {
         amount: 5000,
       })
     )
-  })  
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')!
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
 })
